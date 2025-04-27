b54440e3c357423e93d75330322e72ee1afd4903
yumin <umin030312@naver.com>
2025-04-27 19:32:49 +0900

    styles : 코멘트 변경

---
+++ src/pages/Contactpage.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import { Link } from "react-router";  // assuming you're using react-router for navigation
+
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background: linear-gradient(to bottom, #ffffff, #898989);
+  height: 100%;
+`;
+
+const ContentWrapper = styled.div`
+  flex: 1;
+  height: 100%;
+  background: linear-gradient(to bottom, #ffffff, #D9D9D9);
+
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-top: 20px;
+  margin-left: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #11366d;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #11366d;
+  }
+`;
+
+const ContactPage: React.FC = () => (
+  <PageWrapper>
+    <ContentWrapper>
+      <Header />
+      <ButtonWrapper>
+        <Link to="/privacy-policy">
+          <Button>개인정보처리방침</Button>
+        </Link>
+        <Link to="/service-policy">
+          <Button>서비스이용약관</Button>
+        </Link>
+      </ButtonWrapper>
+    </ContentWrapper>
+  </PageWrapper>
+);
+
+export default ContactPage;
+
+
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Homepage";
 import NoticePage from "../pages/Noticepage"
 import FAQPage from "../pages/FAQpage"
-
+import ContactPage from "../pages/Contactpage"
+import PrivacyPolicy from "../pages/privacy-policy";
+import ServicePolicy from "../pages/service-policy";
 function App() {
   return (
     <Router>
@@ -10,7 +12,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/notice" element={<NoticePage />}/>
         <Route path="/faq" element={<FAQPage />}/>
-        <Route path="/contact" element={<div>Contact Us Page</div>} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/service-policy" element={<ServicePolicy />} />
+        <Route path="/contact" element={<ContactPage />}/>
       </Routes>
     </Router>
   );
